1b68ba8f4bf79977364e6fac576d2a4d21a59c21
Kacper <kacperodziemczyk@gmail.com>
2023-07-05 21:25:03 +0200

    data edited

--- server.js
+++ server.js
@@ -1,32 +1,66 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
-const PORT = 5000;
+const PORT = 3001;
 
-const registeredUsers = [
-  {
-    name: 'admin',
-    password: 'admin',
-    email: 'emailadmin',
-  },
-];
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors()); // Add this line to enable CORS
 app.use(bodyParser.json());
-app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('<p>Welcome to the home page!</p>');
+let dataObjects = [
+  {
+    username: 'Rycho',
+    password: '123456',
+    email: 'kacper@gmail.com',
+  },
+]; // Array to store the data objects received
+
+app.post('/api/data', (req, res) => {
+  const newData = req.body;
+
+  // Check if the user profile exists
+  const userExists = dataObjects.some((obj) => obj.username === newData.username);
+
+  if (userExists) {
+    // User profile exists, perform the necessary actions
+    // For example, you can navigate to the profile page
+    res.status(200).json({ success: true, message: 'User profile exists' });
+  } else {
+    // User profile doesn't exist, handle the appropriate error or display a message
+    res.status(404).json({ success: false, message: 'User profile does not exist' });
+  }
 });
 
-app.post('/register', (req, res) => {
-  const { name, password, email } = req.body;
-  const user = { name, password, email };
-  registeredUsers.push(user);
+app.post('/api/data', (req, res) => {
+  const newData = req.body;
+  dataObjects.push(newData);
+  console.log(JSON.stringify(dataObjects, null, 2));
+  console.log('Data received:', newData);
+  res.sendStatus(200); // Send a success status code
+});
 
-  res.status(200).json({ message: 'Account created successfully' });
+app.delete('/api/data/:index', (req, res) => {
+  const { index } = req.params;
+
+  // Check if the index is valid
+  if (index >= 0 && index < dataObjects.length) {
+    // Remove the object at the specified index
+    dataObjects.splice(index, 1);
+
+    // Send a response indicating success
+    res.json({ success: true });
+  } else {
+    // Send a response indicating failure
+    res.status(404).json({ success: false, message: 'Object not found' });
+  }
+});
+
+app.get('/api/data', (req, res) => {
+  res.json(dataObjects); // Send the stored data objects as a response
+});
+
+app.get('/', (req, res) => {
+  res.send('<p>Welcome to the home page!</p>'); // Send a simple HTML response
 });
 
 app.listen(PORT, () => {
